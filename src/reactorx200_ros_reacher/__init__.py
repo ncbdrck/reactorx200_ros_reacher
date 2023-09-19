06641d7e313f7dfce2a8b5175224385b109e7dee
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-09-19 20:45:53 +0100

    added v2 envs

--- src/reactorx200_ros_reacher/__init__.py
+++ src/reactorx200_ros_reacher/__init__.py
@@ -8,42 +8,42 @@ from gym.envs.registration import register
 register(
     id='RX200ReacherEnvSim-v0',
     entry_point='reactorx200_ros_reacher.sim.task_envs.reactorx200_reacher_sim:RX200ReacherEnv',
-    max_episode_steps=100,
+    max_episode_steps=1000,
 )
 
 # RX200 Reacher Multiros Goal Environment
 register(
     id='RX200ReacherGoalEnvSim-v0',
     entry_point='reactorx200_ros_reacher.sim.task_envs.reactorx200_reacher_goal_sim:RX200ReacherGoalEnv',
-    max_episode_steps=100,
+    max_episode_steps=1000,
 )
 
 # RX200 Reacher Multiros Default Environment v1
 register(
     id='RX200ReacherEnvSim-v1',
     entry_point='reactorx200_ros_reacher.sim.task_envs.reactorx200_reacher_sim_v1:RX200ReacherEnv',
-    max_episode_steps=100,
+    max_episode_steps=1000,
 )
 
 # RX200 Reacher Multiros Goal Environment v1
 register(
     id='RX200ReacherGoalEnvSim-v1',
     entry_point='reactorx200_ros_reacher.sim.task_envs.reactorx200_reacher_goal_sim_v1:RX200ReacherGoalEnv',
-    max_episode_steps=100,
+    max_episode_steps=1000,
 )
 
 # RX200 Reacher Multiros Default Environment v2
 register(
     id='RX200ReacherEnvSim-v2',
     entry_point='reactorx200_ros_reacher.sim.task_envs.reactorx200_reacher_sim_v2:RX200ReacherEnv',
-    max_episode_steps=100,
+    max_episode_steps=1000,
 )
 
 # RX200 Reacher Multiros Goal Environment v2
 register(
     id='RX200ReacherGoalEnvSim-v2',
     entry_point='reactorx200_ros_reacher.sim.task_envs.reactorx200_reacher_goal_sim_v2:RX200ReacherGoalEnv',
-    max_episode_steps=100,
+    max_episode_steps=1000,
 )
 
 
