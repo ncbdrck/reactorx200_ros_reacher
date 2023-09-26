01fdbb980e81934f96d71e3b34f2776eb642ffd9
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-09-26 22:16:09 +0100

    added seed

--- src/reactorx200_ros_reacher/sim/task_envs/reactorx200_reacher_sim.py
+++ src/reactorx200_ros_reacher/sim/task_envs/reactorx200_reacher_sim.py
@@ -220,7 +220,7 @@ class RX200ReacherEnv(reactorx200_robot_sim.RX200RobotEnv):
             np.array([self.position_goal_min["x"], self.position_goal_min["y"], self.position_goal_min["z"]]))
 
         # -- goal space for sampling
-        self.goal_space = spaces.Box(low=low_goal_pos_range, high=high_goal_pos_range, dtype=np.float32)
+        self.goal_space = spaces.Box(low=low_goal_pos_range, high=high_goal_pos_range, dtype=np.float32, seed=seed)
 
         """
         Define subscribers/publishers and Markers as needed.
