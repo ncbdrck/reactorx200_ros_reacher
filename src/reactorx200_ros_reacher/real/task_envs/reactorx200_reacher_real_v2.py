91604457ba898f8f222ed1b6cc469503cdefbc5f
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-10-07 22:09:47 +0100

    fixed the default roscore

--- src/reactorx200_ros_reacher/real/task_envs/reactorx200_reacher_real_v2.py
+++ src/reactorx200_ros_reacher/real/task_envs/reactorx200_reacher_real_v2.py
@@ -29,6 +29,7 @@ This is the v2 of the RX200 Reacher Task Environment. Following are the new feat
     * No EE action space - we are using joint action space
 """
 
+
 class RX200ReacherEnv(reactorx200_robot_real_v1.RX200RobotEnv):
     """
     This Task env is for a simple Reach Task with the RX200 robot.
@@ -53,13 +54,14 @@ class RX200ReacherEnv(reactorx200_robot_real_v1.RX200RobotEnv):
         * environment_loop_rate: Rate at which the environment loop should run. (in Hz)
         * action_cycle_time: Time to wait between two consecutive actions. (in seconds)
         * use_smoothing: Whether to use smoothing for actions or not.
+        * default_port: Whether to use the default port for the roscore or not.
     """
 
     def __init__(self, new_roscore: bool = False, roscore_port: str = None, seed: int = None,
                  close_env_prompt: bool = True, reward_type: str = "Dense",
                  delta_action: bool = True, delta_coeff: float = 0.05, real_time: bool = True,
                  environment_loop_rate: float = None, action_cycle_time: float = 0.0,
-                 use_smoothing: bool = False):
+                 use_smoothing: bool = False, default_port=True):
 
         """
         variables to keep track of ros port
@@ -70,8 +72,12 @@ class RX200ReacherEnv(reactorx200_robot_real_v1.RX200RobotEnv):
         Initialise the env
         """
 
+        # launch a new roscore with default port
+        if default_port:
+            ros_port = self._launch_roscore(default_port=default_port)
+
         # Launch new roscore
-        if new_roscore:
+        elif new_roscore:
             ros_port = self._launch_roscore(port=roscore_port)
 
         # ros_port of the already running roscore
@@ -779,7 +785,8 @@ class RX200ReacherEnv(reactorx200_robot_real_v1.RX200RobotEnv):
     # ------------------------------------------------------
     #   Task Methods for launching roscore
 
-    def _launch_roscore(self, port=None, set_new_master_vars=False):
+    @staticmethod
+    def _launch_roscore(port=None, set_new_master_vars=False, default_port=False):
         """
         Launches a new roscore with the specified port. Only updates the ros_port.
 
@@ -787,7 +794,11 @@ class RX200ReacherEnv(reactorx200_robot_real_v1.RX200RobotEnv):
             ros_port: port of launched roscore
         """
 
-        ros_port, _ = ros_common.launch_roscore(port=int(port), set_new_master_vars=set_new_master_vars)
+        if port is None:
+            port = int(port)
+
+        ros_port = ros_common.launch_roscore(port=port, set_new_master_vars=set_new_master_vars,
+                                             default_port=default_port)
 
         # change to new rosmaster
         ros_common.change_ros_master(ros_port)
