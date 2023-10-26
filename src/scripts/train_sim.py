a92673d90049c6266272d43b04a168f31e15d9c2
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-10-26 23:00:37 +0100

    updated for discrete envs

--- src/scripts/train_sim.py
+++ src/scripts/train_sim.py
@@ -23,6 +23,8 @@ import reactorx200_ros_reacher
 from sb3_ros_support.sac import SAC
 from sb3_ros_support.td3 import TD3
 from sb3_ros_support.td3_goal import TD3_GOAL
+from sb3_ros_support.dqn import DQN
+from sb3_ros_support.dqn_goal import DQN_GOAL
 
 # wrappers
 from multiros.wrappers.normalize_action_wrapper import NormalizeActionWrapper
@@ -41,8 +43,12 @@ if __name__ == '__main__':
     #                delta_action=True, real_time=True, environment_loop_rate=10.0, action_cycle_time=0.8,
     #                use_smoothing=True)
 
-    env = gym.make('RX200ReacherEnvSim-v2', gazebo_gui=False, delta_action=True, real_time=True, reward_type="dense",
-                   environment_loop_rate=10.0, action_cycle_time=0.8, seed=10, use_smoothing=True)
+    # env = gym.make('RX200ReacherEnvSim-v2', gazebo_gui=False, delta_action=True, real_time=True, reward_type="dense",
+    #                environment_loop_rate=10.0, action_cycle_time=0.8, seed=10, use_smoothing=True)
+
+    # # discrete action space
+    # env = gym.make('RX200ReacherDiscreteEnvSim-v0', gazebo_gui=False, delta_coeff=0.05, real_time=True,
+    #                environment_loop_rate=10.0, action_cycle_time=0.8, seed=10, use_smoothing=False, reward_type="dense")
 
     # --- goal-conditioned environments
     # env = multiros_gym.make('RX200ReacherGoalEnvSim-v1', gazebo_gui=False, ee_action_type=False,
@@ -53,15 +59,20 @@ if __name__ == '__main__':
     #                reward_type="sparse", real_time=True, environment_loop_rate=10.0, action_cycle_time=0.8, seed=10,
     #                use_smoothing=False)
 
+    # discrete action space
+    env = gym.make('RX200ReacherDiscreteGoalEnvSim-v0', gazebo_gui=False, delta_coeff=0.05, real_time=True,
+                   environment_loop_rate=10.0, action_cycle_time=0.8, seed=10, use_smoothing=True,
+                   reward_type="sparse")
+
     # Normalize action space
-    env = NormalizeActionWrapper(env)
+    # env = NormalizeActionWrapper(env)
 
     # Normalize observation space
     # env = NormalizeObservationWrapper(env)
     env = NormalizeObservationWrapper(env, normalize_goal_spaces=True)  # goal-conditioned environments
 
     # Set max steps
-    termination_action = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+    # termination_action = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
     # env = TimeLimitWrapper(env, max_steps=100, termination_action=termination_action)
     env = TimeLimitWrapper(env, max_steps=100)
 
@@ -76,18 +87,27 @@ if __name__ == '__main__':
     # save_path = "/models/sim/sac/"
     # log_path = "/logs/sim/sac/"
 
-    # # create the model
+    # # create the model - SAC
     # model = SAC(env, save_path, log_path, model_pkg_path=pkg_path,
     #             config_file_pkg=pkg_path, config_filename=config_file_name)
 
-    # Default base environments - TD3
-    config_file_name = "td3.yaml"
-    save_path = "/models/sim/td3/"
-    log_path = "/logs/sim/td3/"
+    # # Default base environments - TD3
+    # config_file_name = "td3.yaml"
+    # save_path = "/models/sim/td3/"
+    # log_path = "/logs/sim/td3/"
+    #
+    # # create the model - TD3
+    # model = TD3(env, save_path, log_path, model_pkg_path=pkg_path,
+    #             config_file_pkg=pkg_path, config_filename=config_file_name)
 
-    # create the model
-    model = TD3(env, save_path, log_path, model_pkg_path=pkg_path,
-                config_file_pkg=pkg_path, config_filename=config_file_name)
+    # # Default base environments - discrete action space - DQN
+    # config_file_name = "dqn.yaml"
+    # save_path = "/models/sim/dqn/"
+    # log_path = "/logs/sim/dqn/"
+    #
+    # # create the model - DQN
+    # model = DQN(env, save_path, log_path, model_pkg_path=pkg_path,
+    #             config_file_pkg=pkg_path, config_filename=config_file_name)
 
     # # Goal-conditioned environments - TD3+HER
     # config_file_name = "td3_goal.yaml"
@@ -97,6 +117,15 @@ if __name__ == '__main__':
     # # create the model
     # model = TD3_GOAL(env, save_path, log_path, model_pkg_path=pkg_path,
     #                  config_file_pkg=pkg_path, config_filename=config_file_name)
+
+    # Goal-conditioned environments - DQN+HER
+    config_file_name = "dqn_goal.yaml"
+    save_path = "/models/sim/dqn_goal/"
+    log_path = "/logs/sim/dqn_goal/"
+
+    # create the model
+    model = DQN_GOAL(env, save_path, log_path, model_pkg_path=pkg_path,
+                     config_file_pkg=pkg_path, config_filename=config_file_name)
 
     # train the models
     model.train()
